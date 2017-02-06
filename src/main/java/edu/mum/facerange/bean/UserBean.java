6a652f81bee43804a2d2274edc588c42626dbc16
cuong <stefan.cuongnguyen@gmail.com>
2017-02-06 11:46:02 -0600

    add

---
+++ src/main/java/edu/mum/facerange/bean/UserBean.java
@@ -0,0 +1,13 @@
+package edu.mum.facerange.bean;
+
+public interface UserBean {
+	
+	public String updateComponent();
+	
+	public String post();
+	
+	public String comment();
+	
+	public String like();
+	
+}
