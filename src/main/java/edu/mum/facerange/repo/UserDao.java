bf832583d0ed2f98f5d33cf9a352f1381586fd0e
charles <2kkhenricharles@gmail.com>
2017-02-07 04:42:51 +0530

    db connection

--- src/main/java/edu/mum/facerange/repo/UserDao.java
+++ src/main/java/edu/mum/facerange/repo/UserDao.java
@@ -1,11 +1,12 @@
 package edu.mum.facerange.repo;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import edu.mum.facerange.model.*;
 
 public interface UserDao {
-	public void addUser(User user);
+	public void addUser(User user)throws SQLException;
 	
 	public boolean deleteByEmail(String email);
 	public void updateUser(User user);
