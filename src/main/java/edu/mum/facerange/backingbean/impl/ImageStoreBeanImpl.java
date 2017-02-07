d0c4076ba60b305b9090b5bb3156f55929a96c66
cuong <stefan.cuongnguyen@gmail.com>
2017-02-07 17:39:05 -0600

    add image store dao

--- src/main/java/edu/mum/facerange/backingbean/impl/ImageStoreBeanImpl.java
+++ src/main/java/edu/mum/facerange/backingbean/impl/ImageStoreBeanImpl.java
@@ -1,65 +1,38 @@
 package edu.mum.facerange.backingbean.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.primefaces.model.UploadedFile;
 
-import com.mysql.jdbc.Statement;
-
 import edu.mum.facerange.backingbean.ImageStoreBean;
-import edu.mum.facerange.util.DatabaseUtilities;
+import edu.mum.facerange.repo.ImageStoreDao;
 
 @Named("imageStoreBean")
 @RequestScoped
 public class ImageStoreBeanImpl implements ImageStoreBean {
+
+	@Inject
+	private ImageStoreDao imageStoreDao;
+
 	private UploadedFile file;
 
-	// Store file in the database
 	public Integer storeImage() {
-		// Create connection
+
 		try {
-			// Load driver
-			Connection connection = DatabaseUtilities.getConnection();
-			// Set autocommit to false to manage it by hand
-			connection.setAutoCommit(false);
-
-			// Create the statement object
-			PreparedStatement statement = connection.prepareStatement("INSERT INTO files (file) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
-			// Set file data
-			statement.setBinaryStream(1, file.getInputstream());
-
-			// Insert data to the database
-			statement.executeUpdate();
-			
-			ResultSet generatedKeys = statement.getGeneratedKeys();
-			
-			// Commit & close
-			connection.commit(); // when autocommit=false
-			connection.close();
-
+			int saveImage = imageStoreDao.saveImage(file.getInputstream());
 			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Upload success",
 					file.getFileName() + " is uploaded.");
 			FacesContext.getCurrentInstance().addMessage(null, msg);
-
-			if (generatedKeys.next()) {
-                return generatedKeys.getInt(1);
-            }
-			
+			return saveImage;
 		} catch (Exception e) {
-			e.printStackTrace();
-
-			// Add error message
 			FacesMessage errorMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Upload error", e.getMessage());
 			FacesContext.getCurrentInstance().addMessage(null, errorMsg);
 		}
-		
+
 		return 0;
 
 	}
